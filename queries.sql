57f865d7128e7a05280607419fdb23f25e2f380e
YANG LIU <>
2022-01-18 09:50:13 +0000

    my first commit

---
+++ queries.sql
@@ -0,0 +1,51 @@
+-- write your queries here
+
+-- Join the two tables so that every column and record appears, 
+-- regardless of if there is not an owner_id. 
+
+SELECT * FROM owners FULL JOIN vehicles 
+ON owners.id=vehicles.owner_id;
+
+
+-- look at inner join
+SELECT * FROM owners JOIN vehicles 
+ON owners.id=vehicles.owner_id;
+
+
+
+-- Count the number of cars for each owner. 
+-- Display the owners first_name, last_name and count of vehicles. 
+-- The first_name should be ordered in ascending order.
+SELECT first_name, last_name, COUNT(*)
+FROM owners 
+JOIN vehicles 
+ON owners.id=vehicles.owner_id
+GROUP BY owners.id
+ORDER BY first_name ASC;
+
+
+
+-- Count the number of cars for each owner 
+-- and display the average price for each of the cars as integers. 
+-- Display the owners first_name, last_name, average price 
+-- and count of vehicles. 
+-- The first_name should be ordered in descending order. 
+-- Only display results with more than one vehicle 
+-- and an average price greater than 10000. 
+
+
+
+SELECT first_name, last_name, 
+ROUND(AVG(price)), COUNT(*)
+FROM owners 
+JOIN vehicles 
+ON owners.id=vehicles.owner_id
+GROUP BY owners.id
+HAVING COUNT(*)>1 AND  ROUND(AVG(price)) > 10000
+ORDER BY first_name DESC;
+
+
+
+
+
+
